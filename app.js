eb8e805ccf6536b5d7eecb6a1aec8c40c3c1110a
Vasilii Detinnik <vasilvix@gmail.com>
2021-01-14 23:17:21 +0300

    Added MongoDB connection Added Posts model

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const feedRoutes = require('./routes/feed')
 
+const MONGODB_URI = process.env.MONGODB_URI
 const app = express()
 
 app.use(bodyParser.json())
@@ -15,5 +18,17 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes)
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 app.listen(8080)

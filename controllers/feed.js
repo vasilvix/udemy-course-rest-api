c81d0e5fa69472173229adf1271e5d5dabb8d233
Vasilii Detinnik <vasilvix@gmail.com>
2021-01-14 18:34:48 +0300

    Added CORS headers

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,14 @@
 exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: 'First post', content: 'This is the first post!' }]
+  })
+}
 
+exports.createPost = (req, res, next) => {
+  const { title, content } = req.body
+  // create post in db
+  res.status(201).json({
+    message: 'Post created successfully!',
+    post: { id: new Date().toISOString(), title, content }
+  })
 }
--- routes/feed.js
+++ routes/feed.js
@@ -4,6 +4,10 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts)
+
+// POST /feed/post
+router.post('/post', feedController.createPost)
 
 module.exports = router
